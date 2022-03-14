fa245d5f820137923cb6fd304587f1f8e913004a
gaki-h <walter.thi.gks@gmail.com>
2022-03-14 20:17:12 +0900

    magick導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+    # @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # get 'posts', 
+  # devise_for :users
   root to: 'posts#index'
+  resources :posts
 end
